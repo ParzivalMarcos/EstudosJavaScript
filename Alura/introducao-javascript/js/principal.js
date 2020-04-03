9f6daec7adb142ddafda79df62ce23e0d3dd0d31
Marcos Lima Marinho <marcolim977@gmail.com>
2020-04-02 23:35:26 -0300

    Including "addEventListener"

--- Alura/introducao-javascript/js/principal.js
+++ Alura/introducao-javascript/js/principal.js
@@ -1,26 +1,54 @@
 var titulo = document.querySelector(".titulo")
 titulo.textContent = 'Aparecida Nutricionista'
 
-var paciente = document.querySelector("#primeiro_paciente")
-var peso = paciente.querySelector(".info-peso").textContent
-var altura = paciente.querySelector(".info-altura").textContent
-var imc = paciente.querySelector(".info-imc")
+var pacientes = document.querySelectorAll(".paciente")
 
-var pesoEhValido = true
-var alturaEhValido = true
+for (var i = 0; i < pacientes.length; i++){
+    var paciente = pacientes[i]
 
-if (peso <= 0 || peso >= 1000){
-    alert("Peso invalido!!")
-    pesoEhValido = false
+    var peso = paciente.querySelector(".info-peso").textContent
+    var altura = paciente.querySelector(".info-altura").textContent
+    var imc = paciente.querySelector(".info-imc")
+
+    var pesoEhValido = true
+    var alturaEhValido = true
+
+    if (peso <= 0 || peso >= 1000){
+        imc.textContent = 'Peso Invalido !'
+        pesoEhValido = false
+        paciente.classList.add("paciente-invalido")
+    }
+    
+    if (altura <= 0 || altura >= 4.00){
+        imc.textContent = 'Peso Invalido !'
+        alturaEhValido = false
+        paciente.classList.add("paciente-invalido")
+    }
+
+    if (pesoEhValido && alturaEhValido){
+        var calc_imc = peso / (altura * altura)
+        // imc.innerHTML = calc_imc
+        imc.textContent = calc_imc.toFixed(2)
+    }
 }
 
-if (altura <= 0 || altura >= 4.00){
-    alert("Altura inválida !!")
-    alturaEhValido = false
-}
+// Utilizando uma função anonima
+titulo.addEventListener("click", function(){
+    console.log('Eu fui clicado !!')
+})
 
-if (pesoEhValido && alturaEhValido){
-    var calc_imc = peso / (altura * altura)
-    // imc.innerHTML = calc_imc
-    imc.textContent = calc_imc
-}
+
+var botao = document.querySelector("#adicionar-paciente")
+
+botao.addEventListener("click", function(){
+    alert("Botão clicado !!")
+})
+
+
+botao.addEventListener("mouseenter", function(){
+    botao.classList.add("botao-selecionado")
+})
+
+botao.addEventListener("mouseout", function(){
+    botao.style.background = '#0c8cd3'
+})
