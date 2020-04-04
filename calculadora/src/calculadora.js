c50d6a5df83c947767e29cd15ca3e333bf7f5eb0
Marcos Lima Marinho <marcolim977@gmail.com>
2020-04-04 18:08:21 -0300

    Ajust Calculator

---
+++ calculadora/src/calculadora.js
@@ -0,0 +1,26 @@
+var numero = 0
+var resultado = document.getElementById('visor')
+resultado.value = 0
+
+// Capturando numero clicado
+function getNum(num){
+    resultado.value = num
+}
+
+// Funções de operações matematicas
+function dividir(){
+    numero = resultado.value
+    resultado.value = ''
+}
+
+function multiplicar(){
+    return 0
+}
+
+function subtrair(){
+    return 0
+}
+
+function somar(){
+    return 0
+}
