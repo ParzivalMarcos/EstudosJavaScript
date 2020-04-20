19081bf2b009d51bcd746e0507a75295a20cd588
Marcos Lima Marinho <marcolim977@gmail.com>
2020-04-20 13:22:27 -0300

    Course JS avanced I - method 'querySelector' in $

--- Alura/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ Alura/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -4,5 +4,14 @@ class NegociacaoController{
     adiciona(event){
         event.preventDefault()
         
+        let $ = document.querySelector.bind(document)  // Atribuindo a função 'querySelector' na variavel $
+
+        let inputData = $("#data")
+        let inputQuantidade = $("#quantidade")
+        let inputValor = $("#valor")
+
+        console.log(inputData.value)
+        console.log(inputQuantidade.value)
+        console.log(inputValor.value)
     }
 }
