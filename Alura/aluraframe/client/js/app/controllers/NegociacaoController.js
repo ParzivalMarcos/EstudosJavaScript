b7a755dbee26b44c44487ec0f5f6d1d9fcd3f3cc
Marcos Lima Marinho <marcolim977@gmail.com>
2020-04-20 15:23:18 -0300

    Course JS avanced I - continuing...

--- Alura/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ Alura/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,17 +1,34 @@
 class NegociacaoController{
 
+    constructor(){
+        let $ = document.querySelector.bind(document)  // Atribuindo a função 'querySelector' na variavel $
+        this._inputData = $("#data")
+        this._inputQuantidade = $("#quantidade")
+        this._inputValor = $("#valor")
+
+    }
 
     adiciona(event){
         event.preventDefault()
-        
-        let $ = document.querySelector.bind(document)  // Atribuindo a função 'querySelector' na variavel $
 
-        let inputData = $("#data")
-        let inputQuantidade = $("#quantidade")
-        let inputValor = $("#valor")
+        let data = new Date(...
+            this._inputData.value
+            .split('-')
+            .map((item, indice) => item - indice % 2))
 
-        console.log(inputData.value)
-        console.log(inputQuantidade.value)
-        console.log(inputValor.value)
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        )
+        console.log(negociacao)
     }
-}
+
+        /**
+         * let data = new Date(this._inputData.value.replace(/-/g, ',')) -> Outra forma utilizando expressões regulares
+         * os '...' ja desmembra cada item do split, e passa como parametros separados para Date
+         * Função map() recebe um iteravel e realiza alguma operação em cada item deste iteravel
+         * Na mesma função map(), esta sendo utilizado o conceito de arrows functions
+         * No exemplo, como possui apenas uma instrução, não é necessario utilizar o bloco '{}'
+         */
+    }
